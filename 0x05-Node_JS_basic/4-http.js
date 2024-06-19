f3008df56bf55ff40e00eda9ac4d1f184d0f2406
ChriskenM <chriskenmutuma0727@gmail.com>
2024-06-19 16:26:01 +0000

    fixing eslint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,15 +2,15 @@ const http = require('http');
 
 // create server
 const app = http.createServer((req, res) => {
-	// palin text headder
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	// send response body
-	res.end('Hello Holberton School!');
+  // palin text headder
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // send response body
+  res.end('Hello Holberton School!');
 });
 
 // port to listen
 app.listen(1245, () => {
-	console.log('listening on port 1245');
+  console.log('listening on port 1245');
 });
 
 // export
